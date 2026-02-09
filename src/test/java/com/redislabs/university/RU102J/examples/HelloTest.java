75d29fc65ac4fdaff1938c6d976fca1c9cf64c82
Hsiang-Jen Li <hsiangjenli@gmail.com>
2026-02-09 10:57:28 +0800

    feat(hello redis): implement basic hello test with Redis

--- src/test/java/com/redislabs/university/RU102J/examples/HelloTest.java
+++ src/test/java/com/redislabs/university/RU102J/examples/HelloTest.java
@@ -1,12 +1,22 @@
 package com.redislabs.university.RU102J.examples;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
+import com.redislabs.university.RU102J.HostPort;
 import org.junit.Test;
+import redis.clients.jedis.Jedis;
 
 public class HelloTest {
 
   @Test
-  public void sayHelloBasic() {}
+  public void sayHelloBasic() {
+    Jedis jedis = new Jedis(HostPort.getRedisHost(), HostPort.getRedisPort());
+    jedis.set("hello", "world");
+    String value = jedis.get("hello");
+    System.out.println(value);
+    assertThat(value, is("world"));
+  }
 
   @Test
   public void sayHello() {}
