1988e18a104e160e9d3bbc57d64a67e946d5f990
Hsiang-Jen Li <hsiangjenli@gmail.com>
2026-02-09 11:05:27 +0800

    feat(hello redis): sayHelloThreadSafe

--- src/test/java/com/redislabs/university/RU102J/examples/HelloTest.java
+++ src/test/java/com/redislabs/university/RU102J/examples/HelloTest.java
@@ -6,13 +6,19 @@ import static org.hamcrest.Matchers.is;
 import com.redislabs.university.RU102J.HostPort;
 import org.junit.Test;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
 
 public class HelloTest {
 
   @Test
   public void sayHelloBasic() {
     Jedis jedis = new Jedis(HostPort.getRedisHost(), HostPort.getRedisPort());
-    jedis.set("hello", "world");
+
+    String result = jedis.set("hello", "world");
+    System.out.println(result);
+    assertThat(result, is("OK"));
+
     String value = jedis.get("hello");
     System.out.println(value);
     assertThat(value, is("world"));
@@ -22,5 +28,20 @@ public class HelloTest {
   public void sayHello() {}
 
   @Test
-  public void sayHelloThreadSafe() {}
+  public void sayHelloThreadSafe() {
+    JedisPool jedisPool =
+        new JedisPool(new JedisPoolConfig(), HostPort.getRedisHost(), HostPort.getRedisPort());
+
+    try (Jedis jedis = jedisPool.getResource()) {
+      String result = jedis.set("hello", "world");
+      System.out.println(result);
+      assertThat(result, is("OK"));
+
+      String value = jedis.get("hello");
+      System.out.println(value);
+      assertThat(value, is("world"));
+    }
+
+    jedisPool.close();
+  }
 }
